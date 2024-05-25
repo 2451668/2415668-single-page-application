f19e4eb843019da7d96a661bcd02a815d55704bf
2451668 <2451668@students.wits.ac.za>
2024-05-25 21:27:08 +0200

    Navbar Function

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.brand}>React</div>
+      <div className={styles.links}>
+        <NavLink to="/" className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}>Home</NavLink>
+        <NavLink to="/theory" className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}>Theory</NavLink>
+        <NavLink to="/artwork" className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}>Artwork</NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
