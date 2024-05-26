75ca730c3e368becb992bb8524e0e935f7f3bf75
2451668 <2451668@students.wits.ac.za>
2024-05-26 15:19:53 +0200

    Better Navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Theory from './components/Theory';
 import Artwork from './components/Artwork';
-import Navbar from './components/Navbar';  
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/theory" element={<Theory />} />
