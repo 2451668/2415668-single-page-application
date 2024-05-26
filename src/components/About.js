4f5def4f7d45d2139332ab4f474599e7a9dbf551
2451668 <2451668@students.wits.ac.za>
2024-05-26 16:07:49 +0200

    Additional Styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ const About = () => {
   return (
     <div className={styles.container}>
       <h1>About</h1>
+      <br></br>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <br></br>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
