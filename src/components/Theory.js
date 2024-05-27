10077f791c3792bbb9fa5a4ded4db1e2f7b6da58
2451668 <2451668@students.wits.ac.za>
2024-05-27 14:20:51 +0200

    Formatting

--- src/components/Theory.js
+++ src/components/Theory.js
@@ -6,14 +6,155 @@ const Theory = () => {
     <div className={styles.container}>
       <h1>🌳 Theoretical Work and Essay content</h1>
       <br></br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h3>The main theory content for this semester was the exploration of Net Art and developing an understanding of how it is made, and how we can use what we learned in developing our own bit of net art. Below is my submission for our second semester assignment - the net art essay.</h3>
       <br></br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h2>Net Art</h2>
       <br></br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h3>Much of our theoretical coursework this semester focused on understanding net or web 
+      art—its definition, origins, distinctions from traditional art, and significance as a 
+      worthwhile form of creative expression. Moving on from the theoretical to the practical 
+      with a case study, I would like to speak about what I understand about net art and will be 
+      basing my framework for this essay on a handful of the assigned readings, supplemented 
+      by a handful of some I sourced online.</h3>
       <br></br>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h3>Emerging prominently in the early 1990s, net art utilizes the internet both as a medium 
+      and as a platform for creating, showcasing, and distributing art. Unlike traditional art, 
+      which often relies on tangible materials and physical spaces, net art exists in the digital 
+      realm, accessible via any internet-connected device (Paul, 2003). This shift not only 
+      dismantles physical barriers but also facilitates global engagement and interactivity 
+      (Carrier, 2018). This is unlike scans, photographs, or recordings, which are themselves 
+      representations of another creative work, due to not being able to experience the work 
+      personally (Rush, 2005). </h3>
       <br></br>
+      <h3>Before really getting into the theory behind net art, I had lumped in all digital art as 
+      technically being ‘art on the net’; digital artworks, such as portraits created in Procreate 
+      or models in Blender, represent digital adaptations of traditional art practices (like 
+      painting and sculpting); they are not net art. The Procreate portrait is a callback to 
+      traditional portraiture, with the reposted image we would see online not being the.png 
+      the artist saved on his iPad, which is itself a conversion and export of Procreate's native 
+      file format. These don’t feature the interactive and network-based aspects that make net 
+      art (Greene, 2004). </h3>
+      <br></br>
+      <h3>et art as a genre distinctly leverages the internet, not just as a hosting platform but as an 
+      essential component of artistic expression, uniquely engaging the audience and offering 
+      interactive experiences where viewers can even influence or change the artwork itself. 
+      This interactivity transforms viewers from passive recipients to active participants, 
+      resulting in a shift in the relationship between the artwork and audience (Hertzmann, 
+      2018). Speaking to the temporal and spatial, unlike traditional art, which exists within defined spatial and temporal boundaries, net art can be ephemeral and continuously 
+      evolving (Manovich, 2001). It transcends geographic and temporal limitations and allows 
+      for real-time interactions and updates, which is not a feature of any other conventional 
+      art practice (Carrier, 2018). This property serves as my primary motivation for my chosen 
+      case study for this writing.</h3>
+      <br></br>
+      <h3>"Death Imitates Language" by Harm van den Dorpel is, I believe, a great exemplar of net 
+      art, leveraging code to explore themes of genetic inheritance and mutation through a 
+      unique interactive and generative process. His artwork employs algorithms similar to 
+      those used in genetic selection to create visual art, underscoring the evolving nature of 
+      digital artworks and their capacity for real-time interaction and transformation. </h3>
+      <br></br>
+      <h3>It was launched in 2016 and uses a generative algorithm to create new artwork from a set 
+      of existing images, referred to as 'parents.' These parent images, initially created by van 
+      den Dorpel himself, undergo a digital'recombination' process where the algorithm selects 
+      and merges elements of the parent images to produce new child artworks. This process 
+      mimics biological inheritance but in a digital context, highlighting the intersection of 
+      biology, technology, and art. Additionally, it features some integration of blockchain 
+      technology. Each artwork generated is recorded on a blockchain, which serves not only 
+      to authenticate the piece as a unique work of art but also to track its ownership history. 
+      This use of blockchain, I believe, underscores a critical aspect of net art—its engagement 
+      with digital culture's contemporary issues, such as digital rights, ownership, and the 
+      reproducibility of digital objects. The project is inherently interactive, inviting viewers to 
+      engage with the artwork not just as observers but as participants. Owners of the parent 
+      artworks have the opportunity to allow their pieces to be used in the creation of new 
+      generations, directly influencing the evolution of the artwork series. This sort of 
+      interaction, where the audience impacts the artwork's development, exemplifies the shift 
+      in art consumption from passive observation to active participation, which, as mentioned 
+      before, is a crucial feature of net art (Carrier, 2018; Hertzmann, 2018).  </h3>
+      <br></br>
+      <h3>he evolving nature of the piece is a pivotal characteristic. Unlike traditional art, which 
+      remains static once completed, this artwork is designed to evolve continually. The digital 
+      realm facilitates this dynamic evolution, allowing the artwork to change and grow over 
+      time, reflecting the mutable and transient nature of digital media. I interpreted this as 
+      speaking to the broader themes of net art's capacity to mirror the fluidity and rapid changes inherent in the digital world. I will not spend any time discussing the philosophy 
+      behind what art or an artist's creation is. In general, as those are beyond the scope of this 
+      writing, I will acknowledge that, at least conceptually, van den Dorpel's work probes 
+      some deep philosophical questions about the nature of creation, the role of the artist, and 
+      the impact of technology on traditional practices. It certainly challenges viewers to 
+      reconsider the meaning of originality and creativity in an age dominated by digital 
+      technology and artificial intelligence.  </h3>
+      <br></br>
+      <h3>That was some exposition and context behind the piece, and in this section, I would like 
+      to analyse the work specifically in the framework of net art as we covered it in our 
+      theoretical content and a bit more generally in the context of net art more broadly.  </h3>
+      <br></br>
+      <h3>As stated before, one of the defining features of Death Imitates Language is its 
+      interactive nature, which aligns with Aaron Hertzmann’s discussion on the evolving role 
+      of the viewer in digital art. Hertzmann highlights that contemporary digital technologies 
+      not only alter the way art is produced but also how it is perceived and engaged with by 
+      audiences (Hertzmann, 2018). In van den Dorpel's project, the audience is not merely 
+      observing but actively participating in the creation of new artworks. This engagement is 
+      facilitated by the digital platform, which allows viewers to influence the generative 
+      process by deciding whether their owned 'parent' artworks can be used to produce new 
+      'child' artworks. This mechanism shows a significant shift in the role of the audience 
+      from passive recipients to active contributors, which is a central theme in net art and a 
+      major departure from traditional artistic practices. The use of generative algorithms in 
+      Death Imitates Language offers a good parallel to biological processes, which David 
+      Carrier discusses in terms of digital reproduction's impact on art’s authenticity and 
+      uniqueness (Carrier, 2018). Van den Dorpel's approach—using algorithms that mimic 
+      genetic selection—challenges traditional notions of creativity and authorship. The 
+      artworks generated are unique not only in their aesthetic but also in their conceptual 
+      inception, reflecting the digital age's influence on our understanding of creation and 
+      originality. This process questions the boundaries of artistic creation, suggesting a new 
+      paradigm where the 'artist' is a curator of processes rather than a sole creator of objects. I 
+      won’t speak on the relevance of blockchain in the role of net art, as that is more of a  
+      feature of this work in particular, but I will say that, in some form, it does engage with 
+      today’s culture and broader discussions about copyright, ownership, or simply the “reproducibility of digital objects” (Carrier, 2018). Blockchain is one solution to the 
+      problem with digital art—its ease of duplication and the consequent challenges to 
+      establishing and maintaining value, even if the advents of cryptocurrencies and NFTs 
+      have left a poor impression on the general public. </h3>
+      <br></br>
+      <h3>To take it in a different direction, I would like to spend a passage talking about my own 
+      interpretation of two specific pieces of the collection, or the population as Dorpel has 
+      labelled them on the site. </h3>
+      <br></br>
+      <h3>Above are Eve and Adam, respectively, and I chose these two in particular to talk about 
+      because of the genealogy feature that Dorpel has, which allows one to track the parents 
+      of any given artwork. Interestingly, you are not able to do so with the two above. Adam 
+      and Eve have children, but no parents you can navigate to. I found it extremely 
+      interesting that Eve was completely blank, and Adam was visually the most complex of 
+      all the generations featured in the site. Adam lacks much in the way of colour; the 
+      children's artworks all seem to share this; however, the detail in the Adam piece is not 
+      replicated in any of the child objects and is the first to feature the dotted overlay pattern 
+      that is recurring in much of the population. Eve, by contrast, is the only object that has 
+      one solid fill and has the least amount of detail in any of the featured artworks. Given the 
+      juxtaposition, I suppose Eve might stand as a canvas of sorts, or an ode to the potential 
+      for creation (Paul, 2003). In contrast, Adam could represent or encapsulate the interent’s 
+      evolved state, featuring many layers of complexity and an aggregation of data, visuals, 
+      and code reflecting contemporary digital life (Manovich, 2001). </h3>
+      <br></br>
+      <h3>Death Imitates Language, at least at the time it was released, could be seen as setting the 
+      stage for a broader showcase of generative art, where the creative process is defined not 
+      by the artist’s hand but by an algorithm's capacity to select, combine, and recontextualise. 
+      The offspring emerging from the two progenitors are a testament to the algorithm’s role 
+      as, if not an artist itself, then a mediator between ideas and having these ideas realised. It 
+      embodies the transformative nature of net art, constantly evolving and reshaping itself in 
+      response to digital stimuli (Rush, 2005).  </h3>
+      <br></br>
+      <h3>Carrier, D., 2018. The Work of Art in the Age of the Internet.</h3>
+      <br></br>
+      <h3>Greene, R., 2004. Internet Art. Thames & Hudson.</h3>
+      <br></br>
+      <h3>Hertzmann, A., 2018. Can Computers Create Art? Arts, [online] 7(18). Available at: 
+      https://www.mdpi.com/2076-0752/7/2/18 [Accessed 26 April 2024].</h3>
+      <br></br>
+      <h3>Manovich, L., 2001. The Language of New Media. MIT Press.</h3>
+      <br></br>
+      <h3>Paul, C., 2003. Digital Art. Thames & Hudson.</h3>
+      <br></br>
+      <h3>Rush, M., 2005. New Media in Art. Thames & Hudson.</h3>
+      <br></br>
+      <h3>Van den Dorpel, H., 2016. Death Imitates Language. [online] Available at: 
+      https://deathimitateslanguage.harmvandendorpel.com/ [Accessed 26 April 2024].</h3>
+
     </div>
   );
 };
