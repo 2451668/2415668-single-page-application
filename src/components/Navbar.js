895c52b496e9bca194f916459a9ccf8099d63e75
2451668 <2451668@students.wits.ac.za>
2024-05-28 21:24:52 +0200

    Editing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,38 +43,6 @@ const Navbar = () => {
 
 export default Navbar;
 
-/* references and learning */
 
-// React help
-
-// https://www.w3schools.com/react/default.asp
-// https://www.geeksforgeeks.org/reactjs-router/
-// https://luqmanshaban.medium.com/react-router-a-step-by-step-guide-4c5ec964d2e9
-// https://hygraph.com/blog/routing-in-react
-// https://www.w3schools.com/react/react_usestate.asp
-// https://www.w3schools.com/react/react_usecontext.asp
-// https://reactrouter.com/en/main/components/link
-// https://v5.reactrouter.com/web/guides/quick-start
-// https://www.freecodecamp.org/news/react-router-tutorial/
-// https://www.telerik.com/blogs/react-basics-how-to-use-react-router-v6
-// https://react.dev/reference/react/useContext
-// https://react.dev/reference/react/createContext
-// https://react.dev/reference/react/useState
-// https://react.dev/learn
-// https://www.openai.com
-
-// csss and styling
-
-// https://www.w3schools.com/react/react_css.asp
-// https://reactjs.org/docs/animation.html
-// https://www.react-reveal.com/
-// https://reactcommunity.org/react-transition-group/
-// https://css-tricks.com/snippets/css/shake-css-keyframe-animation/
-// https://www.w3schools.com/howto/howto_css_shake_image.asp
-// https://codepen.io/anon/pen/jWmzyZ
-
-// handling events
-
-// https://www.w3schools.com/react/react_events.asp
 
 
